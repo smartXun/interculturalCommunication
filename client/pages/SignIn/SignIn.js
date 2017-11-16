9dca91d9460a106090a93eace5b48ce6e1da5328
胡义晋 <jingdiqkl123@qq.com>
2017-11-16 12:12:21 +0800

    signin

--- client/pages/SignIn/SignIn.js
+++ client/pages/SignIn/SignIn.js
@@ -1,27 +1,51 @@
-// pages/SignIn/SignIn.js
+var qcloud = require('../../vendor/wafer2-client-sdk/index')
+var config = require('../../config')
+var util = require('../../utils/util.js')
+var app = getApp()
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
+    logged:false
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
   
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
   onReady: function () {
   
   },
-  wechatLogin: ()=>{
-    
+  wechatLogin: function () {
+    util.showBusy('正在登录')
+    var that = this
+    qcloud.login({
+      success(result) {
+        if (result) {
+          util.showSuccess('登录成功')
+          app.globalData.userInfo = result;
+          page.setData({
+            logged: true
+          })
+        } else {
+          // 如果不是首次登录，不会返回用户信息，请求用户信息接口获取
+          qcloud.request({
+            url: config.service.requestUrl,
+            login: true,
+            success(result) {
+              util.showSuccess('登录成功')
+              app.globalData.userInfo = result;
+              that.setData({
+                logged: true
+              })
+            },
+            fail(error) {
+              util.showModel('请求失败', error)
+              console.log('request fail', error)
+            }
+          })
+        }
+      },
+      fail(error) {
+        util.showModel('登录失败', error)
+        console.log('登录失败', error)
+      }
+    })
   }
 })
