681c0a8036843bf386132b1316fd1294aade4a61
胡义晋 <jingdiqkl123@qq.com>
2017-11-20 21:36:28 +0800

    论坛数据表

---
+++ server/tools/forum_topic.sql
@@ -0,0 +1,20 @@
+SET NAMES utf8;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+--  Table structure for `forum_topic`
+-- ----------------------------
+DROP TABLE IF EXISTS `forum_topic`;
+CREATE TABLE `forum_topic` (
+  `id` mediumint(9) NOT NULL auto_increment,
+  `user_id` mediumint(9) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `title` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `content` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `last_visit_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `id` (`id`) USING BTREE,
+  KEY `user_id` (`user_id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='论坛主题';
+
+SET FOREIGN_KEY_CHECKS = 1;
---
+++ server/tools/forum_topic_back.sql
@@ -0,0 +1,21 @@
+SET NAMES utf8;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+--  Table structure for `forum_topic_back`
+-- ----------------------------
+DROP TABLE IF EXISTS `forum_topic_back`;
+CREATE TABLE `forum_topic_back` (
+  `id` mediumint(9) NOT NULL auto_increment,
+  `user_id` mediumint(9) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `topic_id` mediumint(9) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `content` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `last_visit_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `id` (`id`) USING BTREE,
+  KEY `topic_id` (`topic_id`) USING BTREE,
+  KEY `user_id` (`user_id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='论坛主题回复';
+
+SET FOREIGN_KEY_CHECKS = 1;
