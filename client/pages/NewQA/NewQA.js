4b74a9f7de153b95019c9d012031b082996ff84b
胡义晋 <jingdiqkl123@qq.com>
2017-11-22 01:07:39 +0800

    文本编辑

--- client/pages/NewQA/NewQA.js
+++ client/pages/NewQA/NewQA.js
@@ -1,66 +1,89 @@
-// pages/NewQA/NewQA.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
+    pageData:[],
+    isEditing: false,
+    editingIndex: 0,
+    showModalStatus: false
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
   
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
+  changeBlur: function(e){
+    let newData = this.data.pageData
+    const index = this.data.editingIndex
+    if (e.detail.value == '') {
+      newData.splice(index, 1)
+    } else {
+      newData[index].content = e.detail.value
+    }
+    this.setData({
+      pageData: newData,
+      isEditing: false
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
+  edit: function(e){
+    const index = e.currentTarget.dataset.index
+    if (this.data.pageData && this.data.pageData[index]){
+      const etype = this.data.pageData[index].type
+      if (etype == 'image'){
+        return;
+      }
+    }
+    this.setData({
+      isEditing: true,
+      editingIndex: index
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
+  addText: function(){
+    let newData = this.data.pageData
+    newData.push({ index: newData.length, type: 'text', content: '' })
+    this.setData({
+      pageData: newData,
+      isEditing: true,
+      editingIndex: newData.length-1
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
+  addImage: function () {
+    let _this = this;
+    wx.showActionSheet({
+      itemList: ['从相册中选择', '拍照'],
+      itemColor: "#f7982a",
+      success: function (res) {
+        if (!res.cancel) {
+          if (res.tapIndex == 0) {
+            _this.chooseWxImage('album')
+          } else if (res.tapIndex == 1) {
+            _this.chooseWxImage('camera')
+          }
+        }
+      }
+    })
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
+  chooseWxImage: function (type) {
+    let _this = this;
+    wx.chooseImage({
+      sizeType: ['original', 'compressed'],
+      sourceType: [type],
+      success: function (res) {
+        let newData = _this.data.pageData
+        newData.push({ index: newData.length, type: 'image', src: res.tempFilePaths[0] })
+        _this.setData({ pageData: newData })
+      }
+    })
   },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
+  handBlockUp: function (e) {
+    const index = e.target.dataset.index;
+    if (index == 0) return;
+    let newData = this.data.pageData;
+    newData[index-1].index += 1;
+    newData[index].index -= 1;
+    newData.sort(function (a, b) { return a.index - b.index; })
+    this.setData({ pageData: newData })
   },
+  handBlockDown: function (e) {
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  },
+  handBlockClose: function (e) {
+
   }
 })
